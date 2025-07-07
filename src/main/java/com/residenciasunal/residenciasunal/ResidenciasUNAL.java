f762652d5204806ede6bf04b8bf9249271d6c404
Teo <matrodriguezpa@unal.edu.co>
2025-07-07 15:21:44 -0500

    Commit inicial

---
+++ src/main/java/com/residenciasunal/residenciasunal/ResidenciasUNAL.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.residenciasunal.residenciasunal;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ResidenciasUNAL {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
