7455e357aad7965827ad880274b7f9df4b3349d0
Santtiago345 <89760337+Santtiago345@users.noreply.github.com>
2025-07-07 22:53:38 -0500

    Crear la clase LinkedList.java

---
+++ src/main/java/com/residenciasunal/residenciasunal/LinkedList.java
@@ -0,0 +1,92 @@
+package com.residenciasunal.residenciasunal;
+
+public class LinkedList<T> {
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> head;
+    private int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        size++;
+    }
+
+    public boolean remove(T data) {
+        if (head == null) return false;
+
+        if (head.data.equals(data)) {
+            head = head.next;
+            size--;
+            return true;
+        }
+
+        Node<T> current = head;
+        while (current.next != null) {
+            if (current.next.data.equals(data)) {
+                current.next = current.next.next;
+                size--;
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) return null;
+
+        Node<T> current = head;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current.data;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void printAll() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+    }
+}
